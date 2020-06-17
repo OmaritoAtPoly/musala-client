ef39195f3b5d8618d6cdc2d8ddaec2879d61758f
Omar Estrada <omarito4857@gmail.com>
2020-06-17 02:43:49 -0500

    Initial commit for Musala Project

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+
+const App: React.FC = () => (
+  <div className="App">
+    <header className="App-header">
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+);
+
+export default App;

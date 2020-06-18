ae4f08c35e71760e5f13253e4ba300ae24c8d6c1
Asiel Alonso <Asiel Hextiandro>
2020-06-18 11:24:05 -0400

    create Home component

---
+++ src/page/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { AdContent } from '../domain/ad/container/AdContent'
+import Container from '../component/Container'
+
+export const HomePage = () => {
+    return (
+        <Container>
+            < AdContent />
+        </Container>
+    )
+}

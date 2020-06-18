555140b853f5c04a67c9eef23738dce8430b3f7f
Asiel Alonso <Asiel Hextiandro>
2020-06-18 11:22:37 -0400

    create AdRow component

---
+++ src/domain/ad/component/AdRow.tsx
@@ -0,0 +1,42 @@
+import { Button, createStyles, makeStyles } from '@material-ui/core';
+import React from 'react';
+import Section from '../../../component/Section';
+
+interface Props {
+    rankedAds: JSX.Element[];
+    showMore?: boolean;
+    title?: string;
+    subtitle?: string;
+}
+
+export const AdsRow = ({ rankedAds, title, subtitle, showMore = false }: Props) => {
+    const classes = useStyles()
+    return (
+        <Section title={title} description={subtitle}>
+            <div className={classes.row} >{
+                rankedAds.map((ad, index: number) => (
+                    <div key={index} className={classes.item} >
+                        {ad}
+                    </div>
+                ))
+            }
+            </div>
+        </Section>
+    )
+};
+
+const useStyles = makeStyles(() =>
+    createStyles({
+        row: {
+            display: 'flex',
+        },
+        item: {
+            flex: 1,
+            paddingRight: '1rem',
+
+            '&:last-child': {
+                paddingRight: 0,
+            }
+        }
+    })
+);

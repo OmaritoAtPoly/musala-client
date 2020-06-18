bd97bc1819322147d17262aa9a3ba1e26e5316a7
Asiel Alonso <Asiel Hextiandro>
2020-06-18 13:59:22 -0400

    create AdDetail component

---
+++ src/domain/ad/container/AdDetail.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { AdDetail as AdDetailView } from '../component/detail'
+
+export const AdDetail = () => {
+    return (
+        <AdDetailView
+            title={'Lorem Ipsum Dolor'}
+            price={25}
+            image='urlImage'
+            description={
+                `Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, modi! Cumque impedit, quia quidem itaque inventore dolor sed quos, odio soluta tempore eum in? Iste facere similique minus atque eum.
+             Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, modi! Cumque impedit, quia quidem itaque inventore dolor sed quos, odio soluta tempore eum in? Iste facere similique minus atque eum.
+             Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, modi! Cumque impedit, quia quidem itaque inventore dolor sed quos, odio soluta tempore eum in? Iste facere similique minus atque eum.
+             Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, modi! Cumque impedit, quia quidem itaque inventore dolor sed quos, odio soluta tempore eum in? Iste facere similique minus atque eum.
+             Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, modi! Cumque impedit, quia quidem itaque inventore dolor sed quos, odio soluta tempore eum in? Iste facere similique minus atque eum.
+             Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, modi! Cumque impedit, quia quidem itaque inventore dolor sed quos, odio soluta tempore eum in? Iste facere similique minus atque eum.
+             Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, modi! Cumque impedit, quia quidem itaque inventore dolor sed quos, odio soluta tempore eum in? Iste facere similique minus atque eum.
+            `
+            }
+        />
+    )
+}
--- src/page/AdPage.tsx
+++ src/page/AdPage.tsx
@@ -1,20 +1,6 @@
 import React from 'react'
-import { AdDetail } from '../domain/ad/component/detail'
+import { AdDetail } from '../domain/ad/container/AdDetail'
 
 export const AdPage = () => {
-    return <AdDetail
-        title={'Lorem Ipsum Dolor'}
-        price={25}
-        image='urlImage'
-        description={
-            `Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, modi! Cumque impedit, quia quidem itaque inventore dolor sed quos, odio soluta tempore eum in? Iste facere similique minus atque eum.
-             Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, modi! Cumque impedit, quia quidem itaque inventore dolor sed quos, odio soluta tempore eum in? Iste facere similique minus atque eum.
-             Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, modi! Cumque impedit, quia quidem itaque inventore dolor sed quos, odio soluta tempore eum in? Iste facere similique minus atque eum.
-             Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, modi! Cumque impedit, quia quidem itaque inventore dolor sed quos, odio soluta tempore eum in? Iste facere similique minus atque eum.
-             Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, modi! Cumque impedit, quia quidem itaque inventore dolor sed quos, odio soluta tempore eum in? Iste facere similique minus atque eum.
-             Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, modi! Cumque impedit, quia quidem itaque inventore dolor sed quos, odio soluta tempore eum in? Iste facere similique minus atque eum.
-             Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, modi! Cumque impedit, quia quidem itaque inventore dolor sed quos, odio soluta tempore eum in? Iste facere similique minus atque eum.
-            `
-        }
-    />
+    return <AdDetail />
 }

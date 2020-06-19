b7d8a817a9a393534b020b2f38f8fb158afd9539
Omar Estrada Díaz <omarito4857@gmail.com>
2020-06-19 14:29:01 -0400

    With signUp mutation working fine

--- src/domain/user/container/Signup.tsx
+++ src/domain/user/container/Signup.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { SignupForm } from '../component/SignupForm'
+import { useSignUpMutation } from '../../../generate/types'
 
 export type SignupInput = {
     fullName: string;
@@ -17,9 +18,27 @@ export type SignupInitValue = {
 }
 
 const Signup = () => {
-    const handleSignup = (values: SignupInput) => {
-        alert(JSON.stringify(values, null, 2));
-    }
+
+    const [signUpFn, { loading, data }] = useSignUpMutation();
+
+    const handleSignup = useCallback(
+        ({email, fullName, password, phone}) => {
+        signUpFn({
+            variables:{
+                email,
+                password,
+                fullName,
+                phone
+            }
+        })
+    }, [signUpFn])
+
+    if(loading) return <div>Loading...</div>
+    if (data) console.log(data);
+    // const handleSignup = (values: SignupInput) => {
+    //     alert(JSON.stringify(values, null, 2));
+    // }
+
     return <SignupForm initialValues={getInitValue()} onSignup={handleSignup} />
 }
 

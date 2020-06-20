b9d5e8ddd961d8f2f10182a8b4b4bf94548b7df0
Francisco Cobas <fcarocena@gmail.com>
2020-06-20 08:05:57 -0400

    remove unnecesary var

--- src/containers/ContainerPage.tsx
+++ src/containers/ContainerPage.tsx
@@ -5,7 +5,7 @@ import routesDefinition from '../routes/routesDefinition';
 import { loggedIn } from '../utils/auth';
 
 const ContainerPage = ({children}: React.PropsWithChildren<{}>) => {
-  const { location: { pathname }, replace } = useHistory();
+  const { location: { pathname } } = useHistory();
   const redirectToLogin = useMemo(
     () => {
       const currentRoute = routesDefinition.find(({ path })=> path === pathname);

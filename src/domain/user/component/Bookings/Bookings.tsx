f19906a4004b179f54fd45b29fe407f40ca1d66c
mact200590 <mact200590@gmail.com>
2020-06-20 12:40:32 -0400

    refactoring code rigth folder

--- src/domain/user/component/Bookings/Bookings.tsx
+++ src/domain/user/component/Bookings/Bookings.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ItemBooking from './BookingRow'
+import ItemBooking from '../BookingRow/BookingRow'
 import { makeStyles } from '@material-ui/styles'
 
 interface Booking {

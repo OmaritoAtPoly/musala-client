683e06dff14f22f8fbdabb6b41929bbae6eb6daf
Asiel Alonso <Asiel Hextiandro>
2020-06-21 17:19:22 -0400

    refactor TitlePanel component

--- src/domain/ad/component/detail/TitlePanel.tsx
+++ src/domain/ad/component/detail/TitlePanel.tsx
@@ -1,68 +1,25 @@
-import React from 'react'
+import { Theme, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
-import { Typography } from '@material-ui/core';
-import customTheme from '../../../../theme';
-import { rgba } from 'polished';
+import React from 'react';
+import { Ranking } from '../../../../component/Ranking';
 
 interface Props {
     title: string;
-    image: string;
+    ranking: number;
 }
 
-export const TitlePanel = ({ title, image }: Props) => {
-    const style = { backgroundImage: image }
-    const classes = useStyles(style);
+export const TitlePanel = ({ title, ranking }: Props) => {
+    const classes = useStyles();
     return (
-        <div className={classes.root}>
-            <div className={classes.containerTitle} >
-                <div className={classes.wrapTitle} >
-                    <Typography className={classes.title} color='textPrimary' >{title}</Typography>
-                </div>
-            </div>
+        <div className={classes.title} >
+            <Typography color='textPrimary' variant='h3' >{title}</Typography>
+            <Ranking ranking={ranking} />
         </div>
     )
 }
 
-const useStyles = makeStyles({
-    root: ({ backgroundImage }: any) => ({
-        display: 'flex',
-        height: '80vh',
-        backgroundColor: customTheme.color.grayLight4,
-        backgroundSize: 'cover',
-        backgroundPosition: 'top'
-
-    }),
-    containerTitle: {
-        display: 'flex',
-        flexDirection: 'column',
-        transform: 'translate(5%,10%)',
-        marginTop: customTheme.spacing.margin.m5,
-        marginLeft: customTheme.spacing.margin.m5
-    },
-    wrapTitle: {
-        backgroundColor: rgba(customTheme.color.grayLight1, .1),
-        animation: "$slideLeft 1s ease-out",
-        padding: customTheme.spacing.padding.small
-    },
+const useStyles = makeStyles((theme: Theme) => ({
     title: {
-        fontSize: '1.5rem',
-        fontWeight: 400,
-        backfaceVisibility: 'hidden',
-        position: "relative",
-    },
-
-    "@keyframes slideLeft": {
-        "0%": {
-            opacity: 0,
-            transform: "translate(-100px)",
-        },
-        "80%": {
-            transform: "translate(10px)"
-        },
-        "100%": {
-            opacity: 1,
-            transform: "translate(0)"
-        }
+        marginBottom: theme.spacing(2),
     }
-
-});
+}));

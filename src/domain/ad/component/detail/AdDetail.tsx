ef01cc3b9fde86c25ff5c4b9392a8d24e32bb125
Asiel Alonso <Asiel Hextiandro>
2020-06-21 17:46:21 -0400

    Create AdDetail view

--- src/domain/ad/component/detail/AdDetail.tsx
+++ src/domain/ad/component/detail/AdDetail.tsx
@@ -1,6 +1,11 @@
+import { Theme, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
 import React from 'react';
+import customTheme from '../../../../theme';
+import { PER_NIGHT } from '../../../../utils/constants';
+import { BookPanel } from './BookPanel';
 import { DescriptionPanel } from './DescriptionPanel';
-import { PricePanel } from './PricePanel';
+import { PicturePanel } from './PicturePanel';
 import { TitlePanel } from './TitlePanel';
 
 interface Props {
@@ -8,16 +13,45 @@ interface Props {
     description: string;
     image: string;
     price: number;
+    onBook: () => void;
 }
 
-export const AdDetail = ({ title, description, image, price }: Props) => {
-
+export const AdDetail = ({ title, description, image, price, onBook }: Props) => {
+    const classes = useStyles()
     return (
-        <div>
-            <TitlePanel title={title} image={image} />
-            <PricePanel price={price} />
+        <>
+            <div className={classes.container} >
+                <div className={classes.imageContainer} >
+                    <PicturePanel urlImage={image} />
+                </div>
+                <div className={classes.detailContainer}>
+                    <TitlePanel ranking={5} title={title} />
+                    <Typography color='textPrimary' variant='h5' >{`$${price} ${PER_NIGHT}`}</Typography>
+                    <BookPanel onClick={onBook} />
+                </div>
+            </div>
             <DescriptionPanel description={description} />
-        </div>
+        </>
     )
 }
 
+const useStyles = makeStyles((theme: Theme) => ({
+    container: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        flexFlow: 'row wrap',
+        [theme.breakpoints.down('md')]: {
+            flexDirection: 'column'
+        }
+    },
+    imageContainer: {
+        backgroundColor: 'white',
+        padding: theme.spacing(2),
+        flex: 1,
+    },
+    detailContainer: {
+        backgroundColor: customTheme.color.background,
+        padding: theme.spacing(2),
+        flex: 1,
+    }
+}));

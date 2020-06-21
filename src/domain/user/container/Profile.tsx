f551edc2fe5592ab73a25716358d35b6fcab536c
mact200590 <mact200590@gmail.com>
2020-06-21 12:13:07 -0400

    create logic for profile component

--- src/domain/user/container/Profile.tsx
+++ src/domain/user/container/Profile.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import UserProfile from '../component/Profile';
-import { getMockUser } from '../../../utils/mockDatas';
+import { CurrentUserDocument, CurrentUserQuery, CurrentUserQueryVariables } from '../../../generate/types'
+import { useQuery } from '@apollo/react-hooks';
+import { ErrorFieldForm } from '../../../component/ErrorFieldForm'
 
 const Profile = () => {
-    const mockUser = getMockUser()
-    return <UserProfile {...mockUser} />
+    const { data, loading, error } = useQuery<CurrentUserQuery, CurrentUserQueryVariables>(CurrentUserDocument);///
+    if (loading) {
+        return <div>Loading...</div>
+    }
+    if (error) {
+        return <ErrorFieldForm name={"Something was wrong"} />
+    }
+    const prepareData = (data: any) => {
+        return {
+            name: data.currentUser.fullName,
+            email: data.currentUser.email,
+            role: data.currentUser.role,
+            bookingAmount: 30
+        }
+    }
+    return <UserProfile {...prepareData(data)} />
+
 }
 export default Profile
 

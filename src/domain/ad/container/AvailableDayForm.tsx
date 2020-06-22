3236461e89bdb0aaeb1cf03a138a4a2b125644d3
Asiel Alonso <Asiel Hextiandro>
2020-06-22 15:47:16 -0400

    create AvailableDayForm container

---
+++ src/domain/ad/container/AvailableDayForm.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import { AvailableDayDialog } from '../component/AvailableDayDialog'
+import { Moment } from 'moment'
+import { DATE_FORMAT } from '../../../utils/constants'
+import { Range } from '../../../utils/type'
+
+interface Props {
+    adId: string;
+    adTitle: string;
+    adRanking: number;
+    visible: boolean;
+    blockedDays: Moment[];
+    handleShowDialog: () => void;
+}
+
+export const BookingForm = ({ adId, adTitle, adRanking, blockedDays, visible, handleShowDialog }: Props) => {
+    const [range, setRange] = useState<Range>()
+    const [isValidRange, setIsValidRange] = useState<boolean>(true)
+
+    const handleValidRange = () => {
+        setIsValidRange(!isValidRange)
+    }
+
+    const handldeOnRangeChange = (range: Range) => {
+        setRange(range)
+    }
+
+    const onSubmit = () => {
+        setIsValidRange(true);
+        setRange(undefined)
+        handleShowDialog()
+        alert(`adId:${adId},
+         checkIn:${ range?.checkIn?.format(DATE_FORMAT)},
+         checkOut:${ range?.checkOut?.format(DATE_FORMAT)}`
+        )
+    }
+
+    return <AvailableDayDialog
+        adTitle={adTitle}
+        ranking={adRanking}
+        blockedDays={blockedDays}
+        onRangeChanged={handldeOnRangeChange}
+        handleValidRangeAlert={handleValidRange}
+        validRange={isValidRange}
+        onSubmit={onSubmit}
+        onClose={handleShowDialog}
+        range={range}
+        visible={visible}
+    />
+}

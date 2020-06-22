c1fce086a7c45fb18a51647bf1a1cebe33eb1517
mact200590 <mact200590@gmail.com>
2020-06-22 00:09:33 -0400

    add alert component

--- src/domain/user/component/Profile.tsx
+++ src/domain/user/component/Profile.tsx
@@ -1,24 +1,30 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/styles';
 import customTheme from '../../../theme';
-import { Typography } from '@material-ui/core';
+import { Typography, CircularProgress } from '@material-ui/core';
+import Alert from '../../../component/Alert'
 
 interface Props {
     name: string;
     role: string;
     email: string;
+    loading: boolean;
     bookingAmount: number;
+    errorMessage?: string,
+    closeError: () => void;
 }
 
-const UserProfile = ({ name, role, email, bookingAmount }: Props) => {
+const UserProfile = ({ name, role, email, loading, bookingAmount, errorMessage, closeError }: Props) => {
     const classes = useStyles()
     return (
         <div className={classes.container} >
             <div className={classes.avatar} />
+            <div className={classes.loading}>{(loading) && <CircularProgress size={20} />}</div>
             <Typography variant={'h4'} color={'textPrimary'} > {name}</Typography>
             <Typography variant={'body1'} color={'textPrimary'} > {role} </Typography>
             <Typography variant={'h5'} color={'textPrimary'} > {email} </Typography>
             <Typography variant={'body1'} color={'textPrimary'} > {`${bookingAmount} completed bookings `} </Typography>
+            <Alert message={errorMessage} open={!!errorMessage} onClose={closeError} />
         </div>
     )
 }
@@ -38,5 +44,11 @@ const useStyles = makeStyles({
         margin: customTheme.spacing.margin.smaller,
         borderRadius: '50%',
         backgroundColor: customTheme.color.grayLight2
+    },
+    loading: {
+        color: customTheme.color.primary,
+        display: 'flex',
+        justifyContent: "center",
+        alignItems: "center"
     }
 });

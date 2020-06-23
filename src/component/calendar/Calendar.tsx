b9fb3b95abdb9e9ce48672a4256e579350f8cb6c
Asiel Alonso <Asiel Hextiandro>
2020-06-23 18:48:07 -0400

    refactor calendar view

--- src/component/calendar/Calendar.tsx
+++ src/component/calendar/Calendar.tsx
@@ -2,23 +2,24 @@ import React from 'react';
 import { Moment } from 'moment';
 import { Range } from '../../containers/calendar/utils/types';
 import { Year } from '../../containers/calendar/Year';
+import { BlockedDay } from '../../containers/calendar/Calendar';
 
 interface Props {
     currentMonth: Moment;
-    blockedDays: Moment[];
+    blockedDayList: BlockedDay[]
     range: Range | undefined;
     setCurrentMonth: (date: Moment) => void;
     handleOnClickedDay: (date: Moment) => void;
 }
 
-const Calendar = ({ currentMonth, blockedDays, range, setCurrentMonth, handleOnClickedDay }: Props) => {
+const Calendar = ({ currentMonth, blockedDayList, range, setCurrentMonth, handleOnClickedDay }: Props) => {
     return (
         <Year
             currentMonth={currentMonth}
-            blockedDays={blockedDays}
             range={range}
             setCurrentMonth={setCurrentMonth}
             onDayClick={handleOnClickedDay}
+            blockedDayList={blockedDayList}
         />
     )
 }

898724469c2a560216fe03151efb3c92a2cfbe13
mact200590 <mact200590@gmail.com>
2020-06-24 23:11:54 -0400

    add const and show correct button

--- src/domain/ad/component/detail/BookPanel.tsx
+++ src/domain/ad/component/detail/BookPanel.tsx
@@ -1,28 +1,35 @@
-import React from 'react'
+import React from 'react';
 import { makeStyles } from '@material-ui/styles';
 import { Theme } from '@material-ui/core';
 import { PrimaryButton } from '../../../../component/PrimaryButton';
-import { BOOK_NOW } from '../../../../utils/constants';
+import { BOOK_NOW, LOGIN_TO_BOOK } from '../../../../utils/constants';
 
 interface Props {
-    onClick: () => void;
-    userId?: string;
+  onClick: () => void;
+  userId?: string;
 }
 
 export const BookPanel = ({ userId, onClick }: Props) => {
-    const classes = useStyles()
-    return (
-        <div className={classes.button} >
-            {
-             userId && <PrimaryButton onClick={onClick} variant='outlined' size='small'>{BOOK_NOW}</PrimaryButton>
-            }
-        </div>
-    )
-}
+  const classes = useStyles();
+
+  return (
+    <div className={classes.button}>
+      {userId ? (
+        <PrimaryButton onClick={onClick} variant="outlined" size="small">
+          {BOOK_NOW}
+        </PrimaryButton>
+      ) : (
+        <PrimaryButton onClick={onClick} variant="outlined" size="small">
+          {LOGIN_TO_BOOK}
+        </PrimaryButton>
+      )}
+    </div>
+  );
+};
 
 const useStyles = makeStyles((theme: Theme) => ({
-    button: {
-        marginTop: theme.spacing(2),
-        display: 'inline-block'
-    }
+  button: {
+    marginTop: theme.spacing(2),
+    display: 'inline-block',
+  },
 }));
